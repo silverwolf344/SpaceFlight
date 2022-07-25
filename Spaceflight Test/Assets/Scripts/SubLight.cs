a1a9b44ec7415ffdbdcae739d7465dd144de82bf
Cameron Moore-Carter <silverwolf442@gmail.com>
2022-07-25 22:56:14 +1200

    Did a little blender to make a cone shape, then made a new shadergraph and material to create a sort of cloudy effect for the SubLight speed. Going to add camera shake and chomatic abberation next.

--- Spaceflight Test/Assets/Scripts/SubLight.cs
+++ Spaceflight Test/Assets/Scripts/SubLight.cs
@@ -9,12 +9,15 @@ public class SubLight : MonoBehaviour
     private bool warpActive = false;
     [SerializeField, Range(0f,1f)]
     public float rate = 0.02f;
+    public MeshRenderer cylinder;
+    public float delay = 2.5f;
     // Start is called before the first frame update
     void Awake()
     {
         warpSpeedVFX = GetComponent<VisualEffect>();
         warpSpeedVFX.Stop();
         warpSpeedVFX.SetFloat("Warp Amount", 0);
+        cylinder.material.SetFloat("Active_", 0);
     }
 
     // Update is called once per frame
@@ -27,17 +30,19 @@ public class SubLight : MonoBehaviour
     {
         if (context.started)
         {
-            Debug.Log("Space Pressed");
             warpActive = !warpActive;
             StartCoroutine(ActivateParticles());
+            StartCoroutine(ActivateShader());
         }
         
     }
 
     IEnumerator ActivateParticles()
     {
+        
         if (warpActive)
         {
+            
             warpSpeedVFX.Play();
             float amount = warpSpeedVFX.GetFloat("Warp Amount");
             while(amount < 1 && warpActive)
@@ -63,6 +68,38 @@ public class SubLight : MonoBehaviour
                 }
             }
             
+        }
+    }
+
+    IEnumerator ActivateShader()
+    {
+        if (warpActive)
+        {
+            yield return new WaitForSeconds(delay);
+            float amount = cylinder.material.GetFloat("Active_");
+            while (amount < 1 && warpActive)
+            {
+                amount += rate;
+                cylinder.material.SetFloat("Active_", amount);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        else
+        {
+            float amount = cylinder.material.GetFloat("Active_");
+            while (amount > 0 && !warpActive)
+            {
+                amount -= rate;
+                cylinder.material.SetFloat("Active_", amount);
+                yield return new WaitForSeconds(0.1f);
+
+                if (amount <= 0 + rate)
+                {
+                    amount = 0;
+                    cylinder.material.SetFloat("Active_", amount);
+                }
+            }
+
         }
     }
 
